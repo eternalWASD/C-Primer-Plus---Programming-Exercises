a33eca7738194b704d289e482b12e3d30ad72b94
eternal <iventus2017@gmail.com>
2020-07-24 10:02:04 +0300

    7-2 Solved

--- 7 - C Control Statements Branching and Jumps/7-2.c
+++ 7 - C Control Statements Branching and Jumps/7-2.c
@@ -1,3 +1,9 @@
+/* Write a program that reads input until encountering #. Have the program print each 
+input character and its ASCII decimal code. Print eight character-code pairs per line. 
+Suggestion: Use a character count and the modulus operator ( %) to print a newline 
+character for every eight cycles of the loop. */
+
+
 #include <stdio.h>
 #include <ctype.h>
 
