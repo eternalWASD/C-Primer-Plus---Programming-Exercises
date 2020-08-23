61197fac96f2d2255b9561130871425bbbf5d687
eternalWASD <iventus2017@gmail.com>
2020-08-23 13:18:11 -0400

    Job is done

--- 11 - Character Strings and String Functions/Program 11-16.c
+++ 11 - Character Strings and String Functions/Program 11-16.c
@@ -11,31 +11,52 @@ as if the –p argument had been used. */
 #include <string.h>
 
 int main(int argc, char *argv[])
-{ 
-    int stringlen = strlen(argv[2]);
+{
 
-    if (argc > 3)
-        printf("Usage: %s [-p | -u | -l] <input>", argv[0]);
-    else if(argc == 3 && argv[1][0] != '-')
-        printf("Usage: %s [-p | -u | -l] <input>", argv[0]);
-    
-    switch (argv[1][1])
+    if (argc == 3 && argv[1][0] != '-')
     {
-    case 'p':
-        puts(argv[2]);
-        break;
-    case 'u':
-        for (int i = 0; i < stringlen; i++)
-            putchar(toupper(argv[2][i]));
-        puts("\n");
-        break;
-    case 'l':
-        for (int i = 0; i < stringlen; i++)
-            putchar(tolower(argv[2][i]));
-        puts("\n");
-        break;
-    default:
-        puts(argv[2]);
-        break;
+        printf("Usage: %s [-p | -u | -l] \"input\"\n", argv[0]);
+        return 1;
     }
+    else if (argc == 3)
+    {
+        int stringlen = strlen(argv[2]);
+        
+        switch (argv[1][1])
+        {
+        case 'p':
+            puts(argv[2]);
+            break;
+        case 'u':
+            for (int i = 0; i < stringlen; i++)
+            {
+                if (isalpha(argv[2][i]))
+                    putchar(toupper(argv[2][i]));
+                else
+                    continue;
+            }
+            puts("\n");
+            break;
+        case 'l':
+            for (int i = 0; i < stringlen; i++)
+            {
+                if (isalpha(argv[2][i]))
+                    putchar(tolower(argv[2][i]));
+                else
+                    putchar(argv[2][i]);
+            }
+            puts("\n");
+            break;
+        default:
+            puts(argv[2]);
+            break;
+        }
+    }
+    else
+    {
+        printf("Usage: %s [-p | -u | -l] \"input\"\n", argv[0]);
+        return 1;
+    }
+
+    return 0;
 }
